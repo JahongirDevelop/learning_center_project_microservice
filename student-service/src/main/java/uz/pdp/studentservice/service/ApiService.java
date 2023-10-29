7a13acc2d7be0e5ebe70baa7d7fbb9c503652c9b
Doniyor <dolimov812@gmail.com>
2023-10-29 15:59:54 +0500

    lesson service and mentor service crud operations

--- student-service/src/main/java/uz/pdp/studentservice/service/ApiService.java
+++ student-service/src/main/java/uz/pdp/studentservice/service/ApiService.java
@@ -4,8 +4,8 @@ import org.springframework.web.client.RestTemplate;
 
 
 public class ApiService {
-    private final RestTemplate restTemplate = new RestTemplate();
-    public <T> T getObject(String url, Class<T> responseType) {
+    private static final RestTemplate restTemplate = new RestTemplate();
+    public static <T> T getObject(String url, Class<T> responseType) {
         return restTemplate.getForObject(url, responseType);
 
     }
